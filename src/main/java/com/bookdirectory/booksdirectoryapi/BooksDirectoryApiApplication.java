b2d8aa7f27d0b1f8af38eb18e92924516c0ef0a1
criszav <czavala.dev@gmail.com>
2023-08-17 14:55:07 -0400

    initial commit

---
+++ src/main/java/com/bookdirectory/booksdirectoryapi/BooksDirectoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookdirectory.booksdirectoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksDirectoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksDirectoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookdirectory/booksdirectoryapi/BooksDirectoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookdirectory.booksdirectoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksDirectoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
